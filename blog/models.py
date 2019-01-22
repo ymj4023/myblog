2fa486c3f386a3e7e0f6025206fbe92d0af21a7e
MJ40 <1249137842@qq.com>
2019-01-22 15:00:34 +0800

    step 8

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Blog(models.Model):
 
     title = models.CharField(max_length=50)
     blog_type = models.ForeignKey(BlogType,on_delete=models.DO_NOTHING)
-    context = models.TextField()
+    content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     created_time = models.DateTimeField(auto_now_add=True)
     last_updated_time = models.DateTimeField(auto_now=True)
