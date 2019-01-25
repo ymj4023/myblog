bda5dd291a0d07313ea6f8fddfd5ed743a0abeab
MJ40 <1249137842@qq.com>
2019-01-25 17:11:01 +0800

    step 12

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Blog, BlogType
 def blog_list(request):
     context = {}
     context['blogs'] = Blog.objects.all()
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_list.html', context)
 
 def blog_detail(request, blog_pk):
@@ -14,6 +15,7 @@ def blog_detail(request, blog_pk):
 def blogs_with_type(request, blog_type_pk):
     context = {}
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
-    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
+    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blogs_with_type.html', context)
