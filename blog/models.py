3cbd3b13115ac962aa8fa9eff2a4bbf0f71b7896
MJ40 <1249137842@qq.com>
2019-02-01 19:46:46 +0800

    step 17

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from ckeditor_uploader.fields import RichTextUploadingField
 class BlogType(models.Model):
 
     type_name = models.CharField(max_length=15)
 
     def __str__(self):
         return self.type_name
+    #def blog_count(self):
+    #    return self.blog_set.count()    
 
 
 
@@ -14,7 +16,7 @@ class Blog(models.Model):
 
     title = models.CharField(max_length=50)
     blog_type = models.ForeignKey(BlogType,on_delete=models.DO_NOTHING)
-    content = models.TextField()
+    content = RichTextUploadingField()
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     created_time = models.DateTimeField(auto_now_add=True)
     last_updated_time = models.DateTimeField(auto_now=True)
