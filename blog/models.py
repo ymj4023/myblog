bf0f1420a03ab9c4be763e08f22ddd098e25dc53
MJ40 <1249137842@qq.com>
2019-03-08 17:56:52 +0800

    step 34

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericRelation
+from django.urls import reverse
 from ckeditor_uploader.fields import RichTextUploadingField
 from read_statistics.models import ReadNumExpandMethod,ReadDetail
 
@@ -26,12 +27,14 @@ class Blog(models.Model,ReadNumExpandMethod):
     created_time = models.DateTimeField(auto_now_add=True)
     last_updated_time = models.DateTimeField(auto_now=True)
 
+    def get_url(self):
+        return reverse('blog_detail',kwargs={'blog_pk':self.pk})
 
-
+    def get_email(self):
+        return self.author.email
 
     def __str__(self):
         return "<Blog: %s>" % self.title
-
 
     class Meta():
         ordering = ['-created_time']
--- myblog/settings.py
+++ myblog/settings.py
@@ -176,6 +176,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.qq.com'
 EMAIL_PORT = 25
 EMAIL_HOST_USER = '1249137842@qq.com'
-EMAIL_HOST_PASSWORD = '授权码' # 授权码
+EMAIL_HOST_PASSWORD = 'gcsavehbolplgcci' # 授权码
 EMAIL_SUBJECT_PREFIX = '[彼岸流天的博客]'
 EMAIL_USE_TLS = True  # 与SMTP服务器通信时，是否启动TLS链接(安全链接)
